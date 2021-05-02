b27a1099521af404d1151711f488544a38dc7c49
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2021-05-02 14:58:44 +0100

    src: Add single key method.

---
+++ src/Method 3 - Single Key for ChaCha20 and Robustness Tag/CommittingChaCha20Poly1305/XChaCha20Poly1305.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography;
+using Sodium;
+
+/*
+    Committing ChaCha20-Poly1305: A committing AEAD implementation.
+    Copyright (c) 2021 Samuel Lucas
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+namespace CommittingChaCha20Poly1305
+{
+    public static class XChaCha20Poly1305
+    {
+        /// <summary>Encrypts a message using committing XChaCha20-Poly1305.</summary>
+        /// <remarks>Never reuse a nonce with the same key. A random nonce is recommended.</remarks>
+        /// <param name="message">The message to encrypt.</param>
+        /// <param name="nonce">The 24 byte nonce.</param>
+        /// <param name="key">The 32 byte key.</param>
+        /// <param name="additionalData">Optional additional data to authenticate.</param>
+        /// <returns>The ciphertext and tag.</returns>
+        public static byte[] Encrypt(byte[] message, byte[] nonce, byte[] key, byte[] additionalData = null)
+        {
+            ParameterValidation.Message(message);
+            ParameterValidation.Nonce(nonce, Constants.XChaChaNonceLength);
+            ParameterValidation.Key(key, Constants.KeyLength);
+            byte[] ciphertext = SecretAeadXChaCha20Poly1305.Encrypt(message, nonce, key, additionalData);
+            byte[] tag = Tag.Read(ciphertext);
+            byte[] robustnessTag = GenericHash.Hash(Arrays.Concat(nonce, tag), key, Constants.RobustnessTagLength);
+            return Arrays.Concat(robustnessTag, ciphertext);
+        }
+
+        /// <summary>Decrypts a ciphertext message using committing XChaCha20-Poly1305.</summary>
+        /// <param name="ciphertext">The ciphertext to decrypt.</param>
+        /// <param name="nonce">The 24 byte nonce.</param>
+        /// <param name="key">The 32 byte key.</param>
+        /// <param name="additionalData">Optional additional data to authenticate.</param>
+        /// <returns>The decrypted message.</returns>
+        public static byte[] Decrypt(byte[] ciphertext, byte[] nonce, byte[] key, byte[] additionalData = null)
+        {
+            ParameterValidation.Ciphertext(ciphertext);
+            ParameterValidation.Nonce(nonce, Constants.XChaChaNonceLength);
+            ParameterValidation.Key(key, Constants.KeyLength);
+            byte[] robustnessTag = RobustnessTag.Read(ciphertext);
+            byte[] tag = Tag.Read(ciphertext);
+            byte[] computedRobustnessTag = GenericHash.Hash(Arrays.Concat(nonce, tag), key, Constants.RobustnessTagLength);
+            bool validRobustnessTag = Utilities.Compare(robustnessTag, computedRobustnessTag);
+            return !validRobustnessTag ? throw new CryptographicException() : SecretAeadXChaCha20Poly1305.Decrypt(RobustnessTag.Remove(ciphertext), nonce, key, additionalData);
+        }
+    }
+}
